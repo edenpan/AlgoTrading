70dc840feb2c42d99df95e064944872117a890a1
Chenfei Xu <chenfeixu@outlook.com>
2018-06-06 20:17:50 +0800

    bug fix

--- grabbers/yahoo_finance_market.py
+++ grabbers/yahoo_finance_market.py
@@ -12,28 +12,36 @@ from datetime import datetime
 def get_price(code):
     url = "http://finance.yahoo.com/quote/%s?p=%s"%(code,code)
     response = requests.get(url)
-    sleep(4)
+    sleep(1)
     parser = html.fromstring(response.text)
 
     summary_table = parser.xpath('//div[contains(@data-test,"summary-table")]//tr')
-    quote_price = parser.xpath('//div[contains(@data-test,"quote-header")]//div//span[contains(@class,"Trsdu(0.3s) Fw(b)")]//text()')
+    quote = parser.xpath('//div[contains(@data-test,"quote-header")]//div//span[contains(@class,"Trsdu(0.3s) Fw(b)")]//text()')
 
     summary_data = OrderedDict()
 
     u_time = str(datetime.now())[0:10]
     summary_data.update({'Date':u_time})
     summary_data.update({'Symbol':code[0:4]})
-    summary_data.update({'Nominal price':str(quote_price[0])})
+    
+    if len(quote) > 0:
+        summary_data.update({'Nominal price' : str(quote[0]).strip()})
+    else:
+        summary_data.update({'Nominal price' : 'N/A'})
 
     for table_data in summary_table:
         raw_table_key = table_data.xpath('.//td[contains(@class,"C(black)")]//text()')
         raw_table_value = table_data.xpath('.//td[contains(@class,"Ta(end)")]//text()')
 
         table_key = str(raw_table_key[0]).strip()
-        table_value = str(raw_table_value[0]).strip()
+        if raw_table_value[0] == u'\u221e':
+            table_value = 'N/A'
+        else:
+            table_value = str(raw_table_value[0]).strip()
         summary_data.update({table_key:table_value})
    
     return summary_data
+
 
 if __name__=="__main__":
     argparser = argparse.ArgumentParser()
