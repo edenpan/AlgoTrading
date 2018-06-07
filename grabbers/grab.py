c71c3fead60fe09ef960fe6a66e4044b64974592
Chenfei Xu <chenfeixu@outlook.com>
2018-06-07 10:30:16 +0800

    Add files via upload

---
+++ grabbers/grab.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+import os
+
+os.system("python ./yahoo_finance_HSI.py")
+os.system("python ./google_finance_HSI.py")
+os.system("python ./etnet_finance_HSI.py")
+#os.system("python ./etnet_finance_HSI.py")
