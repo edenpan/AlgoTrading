ea830f8f44b3edba0f32e0b54be10fae698ffbae
Chenfei Xu <chenfeixu@outlook.com>
2018-06-07 10:23:37 +0800

    Update etnet_finance_HSI.py

--- grabbers/etnet_finance_HSI.py
+++ grabbers/etnet_finance_HSI.py
@@ -8,6 +8,7 @@ from time import sleep
 from collections import OrderedDict
 import pandas as pd
 from datetime import datetime
+import os
 
 def get_price(code):
     
@@ -116,12 +117,15 @@ if __name__=="__main__":
         summary_data = get_price(code)
         price_data = pd.DataFrame.from_dict(summary_data, orient='index').T       
         HSI_price_data = pd.concat([HSI_price_data, price_data], sort=True)
+    
+    directory = updated_time
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    file_name = directory + '/HSI_etnet_' + updated_time
 
     file_name = 'HSI_etnet_' + updated_time
     HSI_price_data.to_csv(file_name + '.csv', sep=',', na_rep='N/A', columns=cols, index=False)
-
-
-
 
 
 
